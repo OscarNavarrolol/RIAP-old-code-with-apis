5e927bbb6d3fab9edbdee96b6912c8b47d9e0859
ManuelValme <manuelvalme2017@gmail.com>
2024-03-16 17:00:56 -0500

    Modificaciones estilos y funcionalidad por johan

--- src/main/java/com/sena/riap/controller/ControllerAttendance.java
+++ src/main/java/com/sena/riap/controller/ControllerAttendance.java
@@ -1,16 +1,7 @@
 package com.sena.riap.controller;
 
-import com.sena.riap.entities.Attendance;
-import com.sena.riap.entities.UserData;
-import com.sena.riap.service.AttendanceService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/attendance")
--- src/main/resources/static/js/chargeData.js
+++ src/main/resources/static/js/chargeData.js
@@ -1,15 +1,54 @@
-document.getElementById("showUserCourse").addEventListener("click", function() {
-    fetch('/userCourse/listUserCourse')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('tableContainer').innerHTML = data;
-        });
-});
-
-document.getElementById("showProgram").addEventListener("click", function() {
-    fetch('/program/listProgram')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('tableContainer').innerHTML = data;
-        });
-});
+$(document).ready(function () {
+    const $output = $("#data");
+    const $headerTable = $("#headerTable");
+    $("#buttonAttendances").click(function () {
+      $.ajax({
+        url: "http://127.0.0.1:8083/api_attendance/list_attendance",
+        type: "GET",
+        dataType: "json",
+        success: function (response) {
+          $headerTable.html(
+            "<tr><th>ID</th><th>Id Event</th><th>Id Attendance</th><th>attendance Time</th><th>Acciones</th></tr>"
+          );
+          $output.html(
+            response.map(
+              (
+                item
+              ) => `<tr><td>${item.idUser}</td><td>${item.idEvent}</td><td>${item.idAttendance}</td>
+                  <td>${item.attendanceTime}</td></tr>`
+            )
+          );
+        },
+        error: function (xhr, status, error) {
+          // Handle error
+          console.error(xhr.responseText);
+        },
+      });
+    });
+  
+    $("#buttonCourses").click(function () {
+      $.ajax({
+        url: "http://127.0.0.1:8083/api_course/list_course",
+        type: "GET",
+        dataType: "json",
+        success: function (response) {
+          $headerTable.html(
+              "<tr><th>ID</th></tr>"
+            );
+          $output.html(
+            response.map(
+              (
+                item
+              ) => `<tr><td>${item.idUser}</td><td>${item.idEvent}</td><td>${item.idAttendance}</td>
+                  <td>${item.attendanceTime}</td><td>Eliminar</td></tr>`
+            )
+          );
+        },
+        error: function (xhr, status, error) {
+          // Handle error
+          console.error(xhr.responseText);
+        },
+      });
+    });
+  });
+  
