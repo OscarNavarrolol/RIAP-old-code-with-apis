e88ef775f9164a3dc1c280e7056db95623f19158
OscarNavarrolol <129440890+OscarNavarrolol@users.noreply.github.com>
2024-04-19 17:43:20 -0500

    Events web controller methods migrated to API, pending application

--- src/main/java/com/sena/riap/api/EventDataRestController.java
+++ src/main/java/com/sena/riap/api/EventDataRestController.java
@@ -1,10 +1,16 @@
 package com.sena.riap.api;
 
+import com.sena.riap.entities.Attendance;
 import com.sena.riap.entities.EventData;
+import com.sena.riap.entities.UserData;
+import com.sena.riap.service.AttendanceService;
+import com.sena.riap.service.CourseService;
 import com.sena.riap.service.EventDataService;
+import com.sena.riap.service.UserDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -15,6 +21,14 @@ public class EventDataRestController {
     @Autowired
     private EventDataService eventDataService;
 
+    @Autowired
+    private UserDataService userDataService;
+
+    @Autowired
+    private AttendanceService attendanceService;
+
+    @Autowired
+    private CourseService courseService;
 
     @GetMapping("/list_event_data")
     public List<EventData> listEventData() {
@@ -42,5 +56,67 @@ public class EventDataRestController {
     }
 
     // METHODS REQUIRED FOR THE PROJECT
+
+    @GetMapping("/list_event")
+    public List<EventData> listEvents(@RequestParam(required = false) Integer courseNumber) {
+        if (courseNumber != null) {
+            return eventDataService.findEventsByCourseNumber(courseNumber);
+        } else {
+            // Devuelve una lista vacia en caso de error.
+            return Collections.emptyList();
+        }
+    }
+
+    @PostMapping("/create_event")
+    public EventData createNewEvent(@RequestBody EventData eventData, @RequestParam Integer courseNumber) {
+        // Guarda evento en la BD
+        EventData savedEventData = eventDataService.saveEventData(eventData);
+
+        // La lista de aprendices asociados al curso
+        List<UserData> learners = userDataService.getLearnersByCourseNumber(courseNumber);
+
+        // Crea registros de asistencia vacios para cada aprendiz asociado al curso
+        for (UserData learner : learners) {
+            Attendance attendance = new Attendance();
+            attendance.setIdEvent(savedEventData.getIdEvent());
+            attendance.setIdUser(learner.getIdUser());
+            // No establecer la hora de asistencia por ahora
+            attendanceService.saveAttendance(attendance);
+        }
+
+        return savedEventData;
+    }
+
+    @DeleteMapping("/delete_event/{eventId}")
+    public void deleteEvent(@PathVariable Long eventId) {
+        // borra todas las asistencias relacionadas a el evento y luego el evento
+        if (eventId != null) {
+            try {
+                attendanceService.deleteAllByEventId(eventId);
+                eventDataService.deleteEventData(eventId);
+            } catch (Exception e) {
+                System.out.println("Error en el método deleteEvent: " + e);
+            }
+        } else {
+            System.out.println("ID de evento no encontrado");
+        }
+    }
+
+    @GetMapping("/update_event/{eventId}")
+    public EventData getEventDataForUpdate(@PathVariable Long eventId) {
+        return eventDataService.getEventDataById(eventId);
+    }
+
+    @PutMapping("/update_event/{eventId}")
+    public EventData updateEvent(@PathVariable Long eventId, @RequestBody EventData updatedEventData) {
+        return eventDataService.updateEventData(eventId, updatedEventData);
+    }
+
+    @GetMapping("/course_numbers")
+    public List<Integer> allCourseNumbers() {
+        UserData user = userDataService.getLoggedInUser();
+        Long idUser = user.getIdUser();
+        return courseService.getCoursesByUser(idUser);
+    }
 
 }
--- src/main/java/com/sena/riap/api/UserDataRestController.java
+++ src/main/java/com/sena/riap/api/UserDataRestController.java
@@ -48,4 +48,6 @@ public class UserDataRestController {
         return userDataService.loginUser(document,password);
     }
 
+
+
 }
