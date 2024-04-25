1dbb49623ac504bcd1f908ae150b44b8255bbc13
OscarNavarrolol <129440890+OscarNavarrolol@users.noreply.github.com>
2024-04-25 14:25:14 -0500

    fix problem with query in attendance

--- src/main/java/com/sena/riap/repository/AttendanceRepository.java
+++ src/main/java/com/sena/riap/repository/AttendanceRepository.java
@@ -17,10 +17,13 @@ public interface AttendanceRepository extends JpaRepository<Attendance,Long> {
 
     public Attendance findByIdUserAndIdEvent(Long idUser,Long idEvent);
     // buscar las asistencias
-    @Query(value = "SELECT a.id_attendance , a.id_event, a.id_user, a.attendance_time FROM attendance a " +
-            "JOIN event_data e ON a.id_event = e.id_event " +
-            "JOIN course c ON e.id_event = c.id_course " +
-            "WHERE c.number_course =:courseNumber  AND e.date_event =:eventDate", nativeQuery = true)
+    @Query(value = "SELECT a.id_attendance, a.id_event, a.id_user, a.attendance_time " +
+            "FROM attendance a " +
+            "INNER JOIN event_data e ON a.id_event = e.id_event " +
+            "INNER JOIN user_data u ON a.id_user = u.id_user " +
+            "INNER JOIN user_course uc ON u.id_user = uc.id_user " +
+            "INNER JOIN course c ON uc.id_course = c.id_course " +
+            "WHERE c.number_course =:courseNumber AND e.date_event =:eventDate ;", nativeQuery = true)
     List<Attendance> findByCourseAndDate (Integer courseNumber, LocalDate eventDate);
 
 }
