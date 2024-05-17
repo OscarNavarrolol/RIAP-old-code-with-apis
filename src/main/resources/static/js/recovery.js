b2ed7859e4a8d68708277786cdaed6a2c94937dd
ManuelValme <manuelvalme2017@gmail.com>
2024-05-16 20:04:47 -0500

    finish table recovery

--- src/main/resources/static/js/recovery.js
+++ src/main/resources/static/js/recovery.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
         dataType: "json",
         success: function (response) {
           const headers =
-            "<tr><th>Id Recovery</th><th>Recovery Key</th><th>Expiration Date</th><th>ID User</th><th>Actions</th></tr>";
+            "<tr><th>Id Recovery</th><th>ID User</th><th>Recovery Key</th><th>Expiration Date</th><th>Actions</th></tr>";
           $headerTable.html(headers);
           $output.html(response.map(mapRecoveryItem));
         },
@@ -20,7 +20,7 @@ $(document).ready(function () {
     }
   
     function mapRecoveryItem(item) {
-      return `<tr><td>${item.idRecovery}</td><td>${item.recoveryKey}</td><td>${item.expirationDate}</td><td>${item.idUser}</td>
+      return `<tr><td>${item.idRecovery}</td><td>${item.idUser}</td><td>${item.recoveryKey}</td><td>${item.expirationDate}</td>
       <td><button id="view-recovery" class="view-btn" data-id="${item.idRecovery}">
       <img src="/images/iconView.png" class="action"></button><button id="edit-recovery" class="edit-btn" data-id="${item.idRecovery}">
       <img src="/images/iconEdit.png" class="action"></button><button id="delete-recovery" class="delete-btn" data-id="${item.idRecovery}">
@@ -51,9 +51,9 @@ $(document).ready(function () {
         dataType: "json",
         success: function (data) {
           $("#idRecovery").val(data.idRecovery).prop("readonly", true);
+          $("#idUserData").val(data.idUser).prop("readonly", true);
           $("#recoveryKey").val(data.recoveryKey).prop("readonly", true);
           $("#expirationDate").val(data.expirationDate).prop("readonly", true);
-          $("#idUserData").val(data.idUser).prop("readonly", true);
           $(".btnHidden").hide();
         },
         error: function (xhr, status, error) {
@@ -62,41 +62,39 @@ $(document).ready(function () {
       });
     });
   
-    $(document).on("click", "#delete-event", function () {
+    $(document).on("click", "#delete-recovery", function () {
       var ID = $(this).data("id");
-      if (confirm("Are you sure you want to delete this event?")) {
+      if (confirm("Are you sure you want to delete this recovery?")) {
         $.ajax({
-          url: `http://localhost:8083/api_event_data/delete/${ID}`,
+          url: `http://localhost:8083/api_recovery/delete/${ID}`,
           type: "DELETE",
           success: function (response) {
-            alert("The event has been successfully deleted!");
-            loadEventList();
+            alert("The recovery has been successfully deleted!");
+            loadRecoveryList();
           },
           error: function (xhr, status, error) {
             console.error(xhr.responseText);
-            alert("There was an error trying to delete the event.");
+            alert("There was an error trying to delete the recovery.");
           },
         });
       }
     });
   
-    $(document).on("click", "#edit-event", function () {
+    $(document).on("click", "#edit-recovery", function () {
       var ID = $(this).data("id");
-      editEventID = ID;
+      editRecoveryID = ID;
       $("#modalForm").show();
   
       $.ajax({
-        url: `http://localhost:8083/api_event_data/find/${ID}`,
+        url: `http://localhost:8083/api_recovery/find/${ID}`,
         type: "GET",
         dataType: "json",
         success: function (data) {
-          $("#idEventData").val(data.idEvent).prop("readonly", true);
-          $("#dateEvent").val(data.date);
-          $("#location").val(data.location);
-          $("#objective").val(data.objective);
-          $("#startTime").val(data.startTime);
-          $("#endTime").val(data.endTime);
-          $("#save-btn-event").show();
+          $("#idRecovery").val(data.idRecovery).prop("readonly", true);
+          $("#idUserData").val(data.idUser);
+          $("#recoveryKey").val(data.recoveryKey);
+          $("#expirationDate").val(data.expirationDate);
+          $("#save-btn-recovery").show();
           $("#clean-btn").hide();
         },
         error: function (xhr, status, error) {
@@ -106,81 +104,75 @@ $(document).ready(function () {
       });
     });
   
-    $(document).on("click", "#add-btn-event", function () {
-      editEventID = null;
+    $(document).on("click", "#add-btn-recovery", function () {
+      editRecoveryID = null;
       $("#modalForm").show();
   
-      $("#idEventData").hide()
-      $("label[for='idEventData']").hide();
-  
-      $("#dateEvent").val("");
-      $("#location").val("");
-      $("#objective").val("");
-      $("#startTime").val("");
-      $("#endTime").val("");
-  
+      $("#idRecovery").hide()
+      $("label[for='idRecovery']").hide();
+
+      $("#idUser").val("");
+      $("#recoveryKey").val("");
+      $("#expirationDate").val("");
+
       $("#btnHidden").show();
     });
   
-    $(document).on("click", "#save-btn-event", function (event) {
+    $(document).on("click", "#save-btn-recovery", function (event) {
       event.preventDefault();
   
-      var ID = editEventID;
+      var ID = editRecoveryID;
   
       if (ID != null) {
         var formData = {
-          date: $("#dateEvent").val(),
-          location: $("#location").val(),
-          objective: $("#objective").val(),
-          startTime: $("#startTime").val(),
-          endTime: $("#endTime").val(),
+          idUser: $("#idUserData").val(),
+          recoveryKey: $("#recoveryKey").val(),
+          expirationDate: $("#expirationDate").val(),
         };
   
         $.ajax({
-          url: `http://localhost:8083/api_event_data/update/${ID}`,
+          url: `http://localhost:8083/api_recovery/update/${ID}`,
           type: "PUT",
           contentType: "application/json",
           data: JSON.stringify(formData),
           success: function (response) {
-            alert("The event data has been successfully updated.");
+            alert("The recovery data has been successfully updated.");
             $("#modalForm").hide();
-            loadEventList();
+            loadRecoveryList();
           },
           error: function (xhr, status, error) {
             console.error(xhr.responseText);
             alert(
-              "There was an error trying to update the event data."
+              "There was an error trying to update the recovery."
             );
           },
         });
       } else {
         var formData = {
-          date: $("#dateEvent").val(),
-          location: $("#location").val(),
-          objective: $("#objective").val(),
-          startTime: $("#startTime").val(),
-          endTime: $("#endTime").val(),
+          idUser: $("#idUserData").val(),
+          recoveryKey: $("#recoveryKey").val(),
+          expirationDate: $("#expirationDate").val(),
         };
   
         $.ajax({
-          url: "http://localhost:8083/api_event_data/save",
+          url: "http://localhost:8083/api_recovery/save",
           type: "POST",
           contentType: "application/json",
           data: JSON.stringify(formData),
           success: function (response) {
-            alert("The event has been saved successfully.");
+            alert("The recovery has been saved successfully.");
             $("#modalForm").hide();
-            loadEventList();
+            loadRecoveryList();
           },
           error: function (xhr, status, error) {
             console.error(xhr.responseText);
-            alert("There was an error trying to save the event.");
+            alert("There was an error trying to save the recovery.");
           },
         });
       }
     });
   
-    $(document).on("click","#returnTable3", function (){
+    $(document).on("click","#returnTable7", function (){
       $("#modalForm").hide();
     });
   });
