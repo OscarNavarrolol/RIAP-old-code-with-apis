c3b5216f68004b2a7696657524f0b23ea991ae41
ManuelValme <manuelvalme2017@gmail.com>
2024-07-20 19:41:26 -0500

    folder organization

---
+++ src/main/resources/static/js/admin/main.js
@@ -0,0 +1,7 @@
+document.write('<script src="/js/admin/attendance.js"></script>');
+document.write('<script src="/js/admin/course.js"></script>');
+document.write('<script src="/js/admin/event.js"></script>');
+document.write('<script src="/js/admin/program.js"></script>');
+document.write('<script src="/js/admin/userCourse.js"></script>');
+document.write('<script src="/js/admin/userData.js"></script>');
+document.write('<script src="/js/admin/recovery.js"></script>');
--- src/main/resources/static/js/main.js
+++
@@ -1,7 +0,0 @@
-document.write('<script src="/js/attendance.js"></script>');
-document.write('<script src="/js/course.js"></script>');
-document.write('<script src="/js/event.js"></script>');
-document.write('<script src="/js/program.js"></script>');
-document.write('<script src="/js/userCourse.js"></script>');
-document.write('<script src="/js/userData.js"></script>');
-document.write('<script src="/js/recovery.js"></script>');
